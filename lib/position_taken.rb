926fb771c62751eadef56807569d7bb2aa0af5c1
Mendy <mendybeltcohen@gmail.com>
2018-05-30 13:49:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, input)
+   #input == "" || input == " " || input == "X" || input == "O"
+   #board[input]  == " " || input == ""
+    board[input] == "" || board[input] == " " board[input] == nil
+    true
+end
